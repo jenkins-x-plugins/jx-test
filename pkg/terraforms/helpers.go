7fac49ea8900e893249c8fd7b9bf85baeaa1b8ca
James Strachan <james.strachan@gmail.com>
2021-02-21 08:31:50 +0000

    fix: lets delete the terraform pods
    on delete of the test

--- pkg/terraforms/helpers.go
+++ pkg/terraforms/helpers.go
@@ -21,13 +21,13 @@ func DeleteActiveTerraformJobs(ctx context.Context, kubeClient kubernetes.Interf
 	jobInterface := kubeClient.BatchV1().Jobs(ns)
 	job, err := jobInterface.Get(ctx, name, metav1.GetOptions{})
 	if err != nil && apierrors.IsNotFound(err) {
-		return nil
+		return deleteTerraformPods(ctx, kubeClient, ns, name)
 	}
 	if err != nil {
 		return errors.Wrapf(err, "failed to query Job %s in namespace %s", name, ns)
 	}
 	if job == nil || jobs.IsJobFinished(job) {
-		return nil
+		return deleteTerraformPods(ctx, kubeClient, ns, name)
 	}
 	log.Logger().Infof("deleting terraform apply Job %s in namespace %s as has not finished and we are about to delete the Terraform resource", info(name), ns)
 	err = jobInterface.Delete(ctx, name, metav1.DeleteOptions{})
@@ -35,5 +35,32 @@ func DeleteActiveTerraformJobs(ctx context.Context, kubeClient kubernetes.Interf
 		return errors.Wrapf(err, "failed to delete Job %s in namespace %s", name, ns)
 	}
 	log.Logger().Infof("deleted terraform apply Job %s in namespace %s", info(name), ns)
+	return deleteTerraformPods(ctx, kubeClient, ns, name)
+}
+
+func deleteTerraformPods(ctx context.Context, kubeClient kubernetes.Interface, ns, name string) error {
+	selector := "job-name=" + name
+	podInterface := kubeClient.CoreV1().Pods(ns)
+	podList, err := podInterface.List(ctx, metav1.ListOptions{
+		LabelSelector: selector,
+	})
+	if err != nil && apierrors.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return errors.Wrapf(err, "failed to query Pods namespace %s with selector %s", ns, selector)
+	}
+	if podList == nil {
+		return nil
+	}
+
+	for _, pod := range podList.Items {
+		name := pod.Name
+		err = podInterface.Delete(ctx, name, metav1.DeleteOptions{})
+		if err != nil {
+			return errors.Wrapf(err, "failed to delete pod %s", name)
+		}
+		log.Logger().Infof("deleted terraform apply Pod %s in namespace %s", info(name), ns)
+	}
 	return nil
 }
